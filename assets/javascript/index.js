0d350aaed97ce6ec190f75f12c0b10d1a1d08f97
Simone <simomilanesio5@gmail.com>
2020-08-24 18:10:41 +0200

    New look

---
+++ assets/javascript/index.js
@@ -0,0 +1,54 @@
+// Scroll top button 
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 400){
+            $("#topBtn").fadeIn();
+        } else {
+            $("#topBtn").fadeOut();
+        }
+     });
+
+     $("#topBtn").click(function() {
+         $("html,body").animate({
+             scrollTop: 0
+         },800);
+     });
+});
+
+
+let text = ["I'm a pro", "I'm a programmer.", "And this is my portfolio."];
+let word = document.getElementById("arrayTitle");
+let counter = 0;
+setInterval(arrayPro, 2000);
+function arrayPro() {
+    word.innerHTML = text[counter];
+    counter++;
+    if (counter >= text.length) {
+        word.innerHTML = text[2];
+    };
+}; 
+
+// Animated Responsive Skill Bar 
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(document).scrollTop() >= 1000) {
+            $(".bar").each(function() {
+                $(this).find(".bar-inner").animate({
+                    width: $(this).attr("data-width")
+                },2000)
+            });
+        };
+    });
+});
+
+// Get year 
+
+let date = new Date();
+
+let year = date.getFullYear();
+
+$(document).ready(function() {
+    $('#year').text(year);
+});
